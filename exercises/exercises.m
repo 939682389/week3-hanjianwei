aa3ac86073d65cceee6dc2a07c988b6f1f59303f
Jianwei Han <hanjianwei@gmail.com>
2018-10-04 17:30:31 +0800

    Add homework

---
+++ exercises/exercises.m
@@ -0,0 +1,39 @@
+function output = exercises()
+% 完成下列Matlab练习，用正确答案替换掉 I___TODO___I
+I___TODO___I = NaN;
+
+% 1.1 生成一个由31和75之间所有的偶数组成的向量，按照降序排列
+output.evens_between_31_and_75  = I___TODO___I;
+% 1.2 生成数列1, 1/2, 1/3, ...的前100项
+output.fraction_100 = I___TODO___I;
+% 1.3 生成数列0, 1/2, 2/3, 3/4, 4/5, ...的前100项
+output.near_1 = I___TODO___I;
+
+% 2. x和y是两个列向量，其中
+x = [3 2 6 8]';
+y = [4 1 3 5]';
+% 2.1 将x和y的对应元素相加得到一个新的列向量
+output.x_add_y = I___TODO___I;
+% 2.2 将x和y的对应元素相乘得到一个新的列向量
+output.x_mul_y = I___TODO___I;
+% 2.3 将x和y中对应元素求乘方得到一个新的列向量
+output.x_power_y = I___TODO___I;
+% 2.4 将y中每个元素加上x中所有元素的和，得到一个新的列向量
+output.y_add_sum_x = I___TODO___I;
+
+
+% 3. A是一个魔方矩阵，完成下列计算
+A = magic(100);
+
+% 3.1 将A的第一行赋给左边的变量
+output.first_row_A = I___TODO___I;
+% 3.2 将A的对角线赋给左边的变量
+output.dialog_A = I___TODO___I;
+% 3.3 将A的奇数行偶数列赋给左边的变量
+output.odd_rows_even_cols_A = I___TODO___I;
+% 3.4 计算A每行的最大值、求和
+output.row_max_A = I___TODO___I;
+output.row_sum_A = I___TODO___I;
+% 3.5 计算A每个元素平方根的和
+output.sum_sqrt_A = I___TODO___I;
+
